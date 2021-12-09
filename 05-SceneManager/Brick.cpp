f4cd7bb1bb33482d2f67664a7604f8276aba204d
tinpham1510 <tinpham15102000@gmail.com>
2021-12-10 01:00:11 +0700

    add Ani Broken Effect

--- 05-SceneManager/Brick.cpp
+++ 05-SceneManager/Brick.cpp
@@ -4,7 +4,7 @@ void CBrick::Render()
 {
 	CAnimations* animations = CAnimations::GetInstance();
 	animations->Get(ID_ANI_BRICK)->Render(x, y);
-	//RenderBoundingBox();
+	RenderBoundingBox();
 }
 
 void CBrick::GetBoundingBox(float &l, float &t, float &r, float &b)
@@ -13,4 +13,29 @@ void CBrick::GetBoundingBox(float &l, float &t, float &r, float &b)
 	t = y - BRICK_BBOX_HEIGHT/2;
 	r = l + BRICK_BBOX_WIDTH;
 	b = t + BRICK_BBOX_HEIGHT;
+}
+
+void CBrick::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	y += vy * dt;
+	if (state == BRICK_STATE_BROKEN)
+	{
+		b1->SetState(BROKEN_EFFECT_STATE_APPEAR);
+		b2->SetState(BROKEN_EFFECT_STATE_APPEAR);
+	}
+	CGameObject::Update(dt, coObjects);
+	CCollision::GetInstance()->Process(this, dt, coObjects);
+}
+
+void CBrick::SetState(int state)
+{
+	CGameObject::SetState(state);
+	switch (state)
+	{
+	case BRICK_STATE_BROKEN:
+		isDeleted = true;
+		break;
+	default:
+		break;
+	}
 }
--- 05-SceneManager/Brick.h
+++ 05-SceneManager/Brick.h
@@ -3,16 +3,22 @@
 #include "GameObject.h"
 #include "Animation.h"
 #include "Animations.h"
+#include "BrokenEffect.h"
 
 #define ID_ANI_BRICK 10000
 #define BRICK_WIDTH 16
 #define BRICK_BBOX_WIDTH 16
 #define BRICK_BBOX_HEIGHT 16
 
+#define BRICK_STATE_NORMAL	0
+#define BRICK_STATE_BROKEN 100
 class CBrick : public CGameObject {
 public:
-	CBrick(float x, float y) : CGameObject(x, y) {}
+	CBrick(float x, float y) : CGameObject(x, y) { SetState(BRICK_STATE_NORMAL); }
+	BrokenEffect* b1;
+	BrokenEffect* b2;
 	void Render();
-	void Update(DWORD dt) {}
+	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
+	void SetState(int state);
 };
